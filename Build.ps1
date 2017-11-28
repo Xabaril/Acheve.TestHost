8a4c9c53fe81f4ebd411a02244fd499d2f5a69ed
Unai Zorrilla Castro <unai@plainconcepts.com>
2017-11-28 19:28:19 +0100

    Another build.ps1 modification to work with appveyor

--- Build.ps1
+++ Build.ps1
@@ -59,7 +59,7 @@ exec { & dotnet restore }
 Invoke-MSBuild
 
 $revision = @{ $true = $env:APPVEYOR_BUILD_NUMBER; $false = 1 }[$env:APPVEYOR_BUILD_NUMBER -ne $NULL];
-$revision = "{0:D4}" -f [convert]::ToInt32($revision, 10)
+#$revision = "{0:D4}" -f [convert]::ToInt32($revision, 10)
 
 exec { & dotnet test .\tests\UnitTests -c Release }
 
