14b749f6fdb2d911d933630ab3d6b71e411e53b2
Unai Zorrilla Castro <unai@plainconcepts.com>
2017-11-28 19:39:21 +0100

    Added version property group into csproj, removed revision number

--- Build.ps1
+++ Build.ps1
@@ -58,10 +58,7 @@ exec { & dotnet restore }
 
 Invoke-MSBuild
 
-#$revision = @{ $true = $env:APPVEYOR_BUILD_NUMBER; $false = 1 }[$env:APPVEYOR_BUILD_NUMBER -ne $NULL];
-#$revision = "{0:D4}" -f [convert]::ToInt32($revision, 10)
-$revision = "1.0.0"
 
 exec { & dotnet test .\tests\UnitTests -c Release }
 
-exec { & dotnet pack .\src\Acheve.TestHost -c Release -o ..\..\artifacts --version-suffix=$revision }
+exec { & dotnet pack .\src\Acheve.TestHost -c Release -o ..\..\artifacts  }
