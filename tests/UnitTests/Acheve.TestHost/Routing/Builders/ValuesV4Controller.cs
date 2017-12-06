12bc88bf2ca96f23f9a50aa856c3fa9e800fefb2
Unai Zorrilla Castro <unai@plainconcepts.com>
2017-12-06 22:05:46 +0100

    Added support for tilde override in route

---
+++ tests/UnitTests/Acheve.TestHost/Routing/Builders/ValuesV4Controller.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UnitTests.Acheve.TestHost.Builders
+{
+    [Route("api/values")]
+    public class ValuesV4Controller
+        : Controller
+    {
+        [HttpGet("~/get1/{index}")]
+        public IActionResult Get1(int index)
+        {
+            return Ok();
+        }
+
+        [HttpPost("~/post1/{index}")]
+        public IActionResult Post1(int index)
+        {
+            return Ok();
+        }
+
+        [HttpPost("~/put1/{index}")]
+        public IActionResult Put1(int index)
+        {
+            return Ok();
+        }
+
+        [HttpPost("~/delete1/{index}")]
+        public IActionResult Delete1(int index)
+        {
+            return Ok();
+        }
+
+
+        [HttpGet("~/get2/{index}")]
+        public IActionResult Get2(int index,Pagination pagination)
+        {
+            return Ok();
+        }
+
+        [HttpPost("~/post2/{index}")]
+        public IActionResult Post2(int index, Pagination pagination)
+        {
+            return Ok();
+        }
+
+        [HttpPost("~/put2/{index}")]
+        public IActionResult Put2(int index, Pagination pagination)
+        {
+            return Ok();
+        }
+
+        [HttpPost("~/delete2/{index}")]
+        public IActionResult Delete2(int index, Pagination pagination)
+        {
+            return Ok();
+        }
+
+        [Route("~/get3/{index}"),HttpGet]
+        public IActionResult Get3(int index, Pagination pagination)
+        {
+            return Ok();
+        }
+
+        [Route("~/post3/{index}"),HttpPost]
+        public IActionResult Post3(int index, Pagination pagination)
+        {
+            return Ok();
+        }
+
+        [Route("~/put3/{index}"),HttpPut]
+        public IActionResult Put3(int index, Pagination pagination)
+        {
+            return Ok();
+        }
+
+        [Route("~/delete3/{index}"),HttpPost]
+        public IActionResult Delete3(int index, Pagination pagination)
+        {
+            return Ok();
+        }
+    }
+}
