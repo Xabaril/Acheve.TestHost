3a995b8f39b26d5711acd332d5d226ab167e649c
Sergio <sergio11_92@hotmail.com>
2022-03-29 14:22:49 +0200

    Tests using WebApplicationFactory (#58)
    Documentation about GetStarted, WebApplicationFactory and ICollectionFixture (xunit)
    Refactor ValuesTests and ValuesWithHttpClientTests to test also WebApplicationFactory
    
    Co-authored-by: Sergio <sergio.merino@plainconcepts.com>

--- samples/Sample.IntegrationTests/Infrastructure/TestHostFixture.cs
+++ samples/Sample.IntegrationTests/Infrastructure/TestHostFixture.cs
@@ -1,7 +1,7 @@
-﻿using System;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting;
+﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.TestHost;
+using System;
+using System.Threading.Tasks;
 using Xunit;
 
 namespace Sample.IntegrationTests.Infrastructure
--- samples/Sample.IntegrationTests/Infrastructure/WebApplicationFactoryApiCollection.cs
+++ samples/Sample.IntegrationTests/Infrastructure/WebApplicationFactoryApiCollection.cs
@@ -2,8 +2,8 @@
 
 namespace Sample.IntegrationTests.Infrastructure
 {
-    [CollectionDefinition(nameof(ApiCollection))]
-    public class ApiCollection : ICollectionFixture<TestHostFixture>
+    [CollectionDefinition(nameof(WebApplicationFactoryApiCollection))]
+    public class WebApplicationFactoryApiCollection : ICollectionFixture<WebApplicationFactoryFixture>
     {
         // This class has no code, and is never created. Its purpose is simply
         // to be the place to apply [CollectionDefinition] and all the
