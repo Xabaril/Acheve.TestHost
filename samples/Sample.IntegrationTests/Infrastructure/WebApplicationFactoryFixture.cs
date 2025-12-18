9d0259a0bf66d68c36454909be232eeaa61d1f88
Sergio <sergio.merino@plainconcepts.com>
2025-12-18 11:52:59 +0100

    remove obsolete IWebHost and IWebHostBuilder in Sample.IntegrationTests

--- samples/Sample.IntegrationTests/Infrastructure/WebApplicationFactoryFixture.cs
+++ samples/Sample.IntegrationTests/Infrastructure/WebApplicationFactoryFixture.cs
@@ -1,20 +1,21 @@
-﻿using Microsoft.AspNetCore;
-using Microsoft.AspNetCore.Hosting;
+﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.Hosting;
 
 namespace Sample.IntegrationTests.Infrastructure
 {
     public class WebApplicationFactoryFixture : WebApplicationFactory<TestStartup>
     {
-        protected override IWebHostBuilder CreateWebHostBuilder()
+        protected override IHostBuilder CreateHostBuilder()
         {
-            return WebHost.CreateDefaultBuilder();
+            return Host.CreateDefaultBuilder();
         }
 
         protected override void ConfigureWebHost(IWebHostBuilder builder)
         {
-            builder.UseStartup<TestStartup>()
+            builder
+                .UseStartup<TestStartup>()
                 .UseSolutionRelativeContentRoot("samples")
                 .UseTestServer();
         }
